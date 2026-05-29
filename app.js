32ac19b3721e8c720b562df2ce0d0126cc8d2b59
Itzel24194 <al222311518@gmail.com>
2026-05-28 19:34:48 -0600

    primer servidor

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hola mundo como estas");
+import express from 'express';
+import dotenv from 'dotenv'; 
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`escucha desde el puerto ${PORT}`)
+})
+console.log(express);
